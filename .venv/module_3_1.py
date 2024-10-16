c9fc38ad6d4f97333888044dcaebe49a7af5ff30
kpsserg <kpsserg@gmail.com>
2024-10-16 19:50:21 +0300

    1-st redaction

--- .venv/module_3_1.py
+++ .venv/module_3_1.py
@@ -24,4 +24,3 @@ print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-# BinGo
