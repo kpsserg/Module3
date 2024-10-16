8327662ea5484f346b25650c602fb52988b89a06
kpsserg <kpsserg@gmail.com>
2024-10-17 02:02:00 +0300

    2-st redaction

--- .venv/module_3_2.py
+++ .venv/module_3_2.py
@@ -59,4 +59,4 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'Urban.teacher@mail.ru', sender='Urban.Teacher@mail.Ru')
-#The End
+#The End!
