8c20ab78b1cb5f54065071d6e690818ede8d13d8
kpsserg <kpsserg@gmail.com>
2024-10-21 19:35:01 +0300

    Add module_3_4

--- .venv/module_3_4.py
+++ .venv/module_3_4.py
@@ -9,9 +9,7 @@ def single_root_words(a, *args):
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-result3 = single_root_words('rich', '')
 
 print(result1)
 print(result2)
-print(result3)
 
