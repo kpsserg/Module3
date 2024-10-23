fda4d0c17f093e03b54b7f942408c93c3f126b34
kpsserg <kpsserg@gmail.com>
2024-10-23 10:15:43 +0300

    Add module_3_5 с глобальной переменной

--- .venv/module_3_5.py
+++ .venv/module_3_5.py
@@ -1,16 +1,32 @@
 def get_multiplied_digits(number):
-    first = 1
+    global c
     str_number = str(number)
-    if len(str_number) > 1:
-        first = int(str_number[0]) * get_multiplied_digits(int(str_number[1:]))
-        return first
-    elif int(str_number[0]) > 0:    #последний символ больше 0
-        first *= int(str_number[0])
-        return first
-    else:
-        return first
+    if len(str_number) == 1 and c and int(str_number[0]) == 0: #условие для последнего нуля, чтобы не обнулить результат
+        return 1
+    elif len(str_number) == 1:
+        return int(str_number)
+    c = True
+    first = int(str_number[0])
+    return first * get_multiplied_digits(int(str_number[1:]))
 
 
-result = get_multiplied_digits(120120)
-
+c = False
+result = get_multiplied_digits(1012345)
 print(result)
+
+
+# def get_multiplied_digits(number):
+#     first = 1
+#     str_number = str(number)
+#     if len(str_number) > 1:
+#         first = int(str_number[0]) * get_multiplied_digits(int(str_number[1:]))
+#         return first
+#     elif int(str_number[0]) > 0:    #последний символ больше 0
+#         first *= int(str_number[0])
+#         return first
+#     else:   # Возвращаем 1
+#         return first
+#
+#
+# result = get_multiplied_digits(0)
+# print(result)
